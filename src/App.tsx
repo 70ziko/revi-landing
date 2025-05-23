e523501a58d386e8165429cc63236f08909dd702
70ziko <fidler.patryk@gmail.com>
2025-05-23 14:07:34 +0200

    refactor: move GraphBackground component to webgl directory and update import paths

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import Benefits from './components/Benefits';
 import CTA from './components/CTA';
 import Footer from './components/Footer';
 import ErrorBoundary from './components/ErrorBoundary';
-import GraphBackground from './components/GraphBackground';
+import GraphBackground from './components/webgl/GraphBackground';
 
 const theme = createTheme({
   palette: {
